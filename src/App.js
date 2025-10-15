e0f879f094107ca1b43d562d28305b8f2b6712f2
knithin16820-boop <knithin16820@gmail.com>
2025-10-15 11:39:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import React from 'react'
-import { useState } from 'react'
-function App() {
-  const[a,seta]=useState(0);
-  const[b,setb]=useState(0);
-  const [res,setres]=useState(0);
-  let add=()=>{
-    setres(parseInt(a)+parseInt(b));
-    seta('');
-    setb('');
-  }
-  let sub=()=>{
-    setres(parseInt(a)-parseInt(b));
-    seta('');
-    setb('');
-  }
-  let mul=()=>{
-    setres(parseInt(a)*parseInt(b));
-    seta('');
-    setb('');
-  }
-  let div=()=>{
-    setres(parseInt(a)/parseInt(b));
-    seta('');
-    setb('');
-  }
-  return (
-    <div className='outer'>
-      <div className='inner'>
-      <label for="number1">Enter the number1 </label>
-      <input type='text' name="number1" value={a} placeholder='enter the number1' onChange={(e)=>{
-        seta(e.target.value)
-      }}></input>
-         <label for="number2">Enter the number1 </label>
-      <input type='text' name="number2" value={b} placeholder='enter the number2' onChange={(e)=>{
-        setb(e.target.value)
-      }}></input>
-      </div>
-      <h1>Result is {res}</h1>
-      <div className='but'>
-      <button onClick={add}>+</button>
-      <button onClick={sub}>-</button>
-      <button onClick={mul}>*</button>
-      <button onClick={div}>/</button>
-      </div>
-    </div>
-  )
+import React from "react";
+
+function App(props){
+    return(
+        <div>
+            <h1>Functional Component</h1>
+            <h3>Name :{props.name}</h3>
+            <h3>Age :{props.age}</h3>
+            <h3>Branch : {props.branch}</h3>
+            <h3>Roll No : {props.rollno}</h3>
+        </div>
+    )
 }
-//git remote add  origin https://github.com/knithin16820-boop/internal.git
-export default App
+
+export default App;
